e7b20fffaee601e9fc4aa742a619d9081e46e846
admaster21 <admaster1942@gmail.com>
2026-06-10 00:55:54 -1000

    Add crosshair & collect message
    - cross hair is centered with screen
    - Message gets prompt when hovering over collectable resource.

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerInteraction : MonoBehaviour
 {
     public Camera mainCamera; // Reference to the main camera
     public Transform interactionPoint; // The point where the players interactions orgininate from.
+    public TextMeshProUGUI collectPrompt; //Varible for the crosshair collect detection.
+
     public float interactionRange = 3f; // Range within which the player can interact with objects
     public PlayerInventory playerInventory;
     
-
+    void Start()
+    {
+        collectPrompt.gameObject.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +26,7 @@ public class PlayerInteraction : MonoBehaviour
         {
             playerInventory.PrintInventory();
         }
+        UpdateCollectPrompt();//updates crosshair promt message.
     }
 
     void TryInteract()
@@ -33,5 +40,22 @@ public class PlayerInteraction : MonoBehaviour
                 interactable.Interact();
             }
         }
+    }
+
+    //if hovering over rescource message "press E to Collect" will be prompted
+    void UpdateCollectPrompt()
+    {
+        if(Physics.Raycast(interactionPoint.position, interactionPoint.forward, out RaycastHit hit, interactionRange))
+        {
+            Interactable interactable = hit.collider.GetComponentInParent<Interactable>();
+
+            if(interactable != null)
+            {
+                collectPrompt.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        collectPrompt.gameObject.SetActive(false);
     }
 }
